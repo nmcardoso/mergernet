0fcc137571c108b9c453295469ff242c6bf16f06
Natanael <natanael_magalhaes@msn.com>
2021-12-04 11:11:11 -0300

    define path constants

---
+++ mergernet/core/constants.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+
+
+# Commom paths
+SOURCE_ROOT = (Path(__file__).parent / '../').resolve()
+PROJECT_ROOT = SOURCE_ROOT.parent
+DOCS_ROOT = PROJECT_ROOT / 'docs'
+DATA_ROOT = PROJECT_ROOT / 'data'
