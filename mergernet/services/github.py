8759fe466ea9f9722dc287419557aa31dd1b474b
Natanael <natanael_magalhaes@msn.com>
2021-12-10 11:11:11 -0300

    encode content

--- mergernet/services/github.py
+++ mergernet/services/github.py
@@ -18,3 +18,12 @@ class GithubService:
 
   def _get_url(self, route: str) -> str:
     return f'{BASE_URL}/{route}'
+
+
+  def _encode_content(self, content: str) -> str:
+    content_bytes = content.encode(encoding='utf-8')
+    base64_bytes = base64.b64encode(content_bytes)
+    base64_str = base64_bytes.decode('utf-8')
+    return base64_str
+
+
