88463e32feddd5fac883c15ff07df8f9999a3326
Natanael <natanael_magalhaes@msn.com>
2021-12-10 11:11:11 -0300

    append query params

--- mergernet/services/utils.py
+++ mergernet/services/utils.py
@@ -12,3 +12,16 @@ def download_file(remote_url: str, save_path: Path):
     f.write(r.content)
 
 
+
+def append_query_params(url: str, query_params: dict) -> str:
+  index = url.find('?')
+  new_url = ''
+
+  if index > -1:
+    base = url[:index]
+    rest = url[index:] + '&' + '&'.join([f'{k}={v}' for (k, v) in query_params.items()])
+    new_url = base + rest
+  else:
+    new_url = url + '?' + '&'.join([f'{k}={v}' for (k, v) in query_params.items()])
+
+  return new_url
