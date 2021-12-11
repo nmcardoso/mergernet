513a475024cd01c6aebb0970fac4e33b0668db33
Natanael <natanael_magalhaes@msn.com>
2021-12-11 11:11:11 -0300

    github constants

--- mergernet/core/constants.py
+++ mergernet/core/constants.py
@@ -8,3 +8,15 @@ SOURCE_ROOT = (Path(__file__).parent / '../').resolve()
 PROJECT_ROOT = SOURCE_ROOT.parent
 DOCS_ROOT = PROJECT_ROOT / 'docs'
 DATA_ROOT = PROJECT_ROOT / 'data'
+
+
+
+# Github authentication
+GITHUB_USER = os.environ.get('GITHUB_USER', None)
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN', None)
+GITHUB_REPO = os.environ.get('GITHUB_REPO', None)
+GITHUB_PATH = os.environ.get('GITHUB_PATH', 'data/jobs_artifacts')
+GITHUB_BRANCH = os.environ.get('GITHUB_BRANCH', 'main')
+
+
+
