c64b93e1b0e1ec0742c7b0bb1aa87c3ecdafa11e
Natanael <natanael_magalhaes@msn.com>
2022-01-09 11:11:11 -0300

    convnet model

---
+++ mergernet/model/convnet.py
@@ -0,0 +1,9 @@
+from mergernet.core.dataset import MergerNetDataset
+
+class ParametricMember:
+  def __init__(self, dataset: MergerNetDataset):
+    self.dataset = dataset
+
+
+  def train(self):
+    pass
