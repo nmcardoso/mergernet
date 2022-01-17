f5a4eabf26caf617a077d1a539fe8a50ce31b5c2
Natanael <natanael_magalhaes@msn.com>
2022-01-17 11:11:11 -0300

    use object method instead

--- mergernet/services/utils.py
+++ mergernet/services/utils.py
@@ -4,8 +4,7 @@ import requests
 
 
 
-def download_file(remote_url: str, save_path: Path):
-  if not Path.exists(save_path.parent):
+  if not save_path.parent.exists():
     save_path.parent.mkdir(parents=True, exist_ok=True)
   r = requests.get(remote_url, allow_redirects=True)
   with open(str(save_path.resolve()), 'wb') as f:
