7d689d72214275aa5d350713c3340d744ab8782d
Natanael <natanael_magalhaes@msn.com>
2022-01-24 11:11:11 -0300

    delete module

--- mergernet/model/convnet.py
+++
@@ -1,9 +0,0 @@
-from mergernet.core.dataset import MergerNetDataset
-
-class ParametricMember:
-  def __init__(self, dataset: MergerNetDataset):
-    self.dataset = dataset
-
-
-  def train(self):
-    pass
