7f7a785caeb3f5309b3552e72c7c1965f8e9a601
Natanael <natanael_magalhaes@msn.com>
2022-01-25 11:11:11 -0300

    database reimplementation

--- mergernet/core/dataset.py
+++ mergernet/core/dataset.py
@@ -22,212 +22,13 @@ import pandas as pd
 
 
 
-# def parse_rgb_example(serialized_example):
-#   """Parse rgb example."""
-#   feature = {
-#     'label': tf.io.FixedLenFeature([], tf.int64),
-#     'image': tf.io.FixedLenFeature([], tf.string)
-#   }
-#   example = tf.io.parse_single_example(serialized_example, feature)
-#   return tf.io.parse_tensor(example['image'], tf.int64), example['label']
-
-
-def get_bytes_feature(value: str):
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-
-
-def get_int64_feature(value: int):
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-
-
-def get_float_feature(value: float):
-  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
-
-
-
-def write_tfrecord_file(file_path: Path, examples: dict):
-  with tf.io.TFRecordWriter(str(file_path.resolve())) as tfrecord:
-    for i in range(len(examples['image'])):
-      label = examples['label'][i]
-      image_path = examples['image'][i]
-      image_array = load_image(Path(image_path))
-      image_tensor = tf.convert_to_tensor(image_array)
-      image_serialized = tf.io.serialize_tensor(image_tensor).numpy() # serialized string
-
-      features = {
-        'label': get_int64_feature(label),
-        'image': get_bytes_feature(image_serialized)
-      }
-
-      example = tf.train.Example(features=tf.train.Features(feature=features))
-      tfrecord.write(example.SerializeToString())
-
-
-
-def parser(serialized_example):
-  features = {
-    'label': tf.io.FixedLenFeature([], tf.int64),
-    'image': tf.io.FixedLenFeature([], tf.string)
-  }
-
-  example = tf.io.parse_single_example(
-    serialized=serialized_example,
-    features=features
-  )
-
-  image = example['image']
-  label = example['label']
-
-  return tf.io.parse_tensor(image, tf.int64), label
-
-
-
-class DatasetConfig:
-  """Configuration params for dataset."""
-  def __init__(
-    self,
-    download_url: str = None,
-    save_path: Path = None,
-    train_glob: Path = None,
-    validation_glob: Path = None,
-    test_glob: Path = None
-  ):
-    self.download_url = download_url
-    self.save_path = save_path
-    self.train_glob = save_path.parent / train_glob # concatenate folder and glob
-    self.validation_glob = save_path.parent / validation_glob
-    self.test_glob = save_path.parent / test_glob
-
-
-
-class DatasetCreator:
-  def __init__(
-    self,
-    # images_folder: Path,
-    images: List[Path],
-    labels: List[int],
-    # rmag: List[float],
-    # metadata_table: Path,
-    output: Path,
-    n_splits: int = 5,
-    bins: int = 6,
-    # rmag_column: str = 'r',
-    # label_column: str = 'class',
-    # filename_column: str = 'filename'
-  ):
-    self.images = images
-    self.labels = labels
-    # self.rmag = rmag
-    # self.images_folder = images_folder
-    # self.metadata_table = metadata_table
-    self.output = output
-    self.n_splits = n_splits
-    self.bins = bins
-    # self.rmag_column = rmag_column
-    # self.label_column = label_column
-    # self.filename_column = filename_column
-
-
-  def generate(self):
-    kf = StratifiedKFold(n_splits=self.n_splits)
-    for fold, (train_ids, test_ids) in enumerate(kf.split(self.images, self.labels)):
-      img_train = self.images[train_ids]
-      img_test = self.images[test_ids]
-      lbl_train = self.labels[train_ids]
-      lbl_test = self.labels[test_ids]
-
-      train_examples = {
-        'image': img_train,
-        'label': lbl_train
-      }
-
-      test_examples = {
-        'image': img_test,
-        'label': lbl_test
-      }
-
-      write_tfrecord_file(
-        file_path=self.output / f'train_fold_{fold}.tfrecord',
-        examples=train_examples
-      )
-
-      write_tfrecord_file(
-        file_path=self.output / f'test_fold_{fold}.tfrecord',
-        examples=test_examples
-      )
-
-    # for fold in range(self.n_splits):
-    #   train_img_path = splited_dataset['X_train'][fold]
-    #   train_labels = splited_dataset['y_train'][fold]
-    #   test_img_path = splited_dataset['X_test'][fold]
-    #   test_labels = splited_dataset['y_test'][fold]
-
-    #   train_examples = {
-    #     'image': train_img_path,
-    #     'labels': train_labels
-    #   }
-
-    #   test_examples = {
-    #     'image': test_img_path,
-    #     'labels': test_labels
-    #   }
-
-    #   write_tfrecord_file(
-    #     file_path=self.output / f'train_fold_{fold}.tfrecord',
-    #     examples=train_examples
-    #   )
-
-    #   write_tfrecord_file(
-    #     file_path=self.output / f'test_fold_{fold}.tfrecord',
-    #     examples=test_examples
-    #   )
-
-
-  def _generate(self):
-    table = pd.read_csv(self.metadata_table)
-    rmag = table[[self.rmag_column]].to_numpy()
-    filenames = table[[self.id_column]].to_numpy()
-    labels = table[[self.label_column]].to_numpy()
-
-    kf = StratifiedDistributionKFold(n_splits=self.n_splits, bins=self.bins)
-    splited_dataset = kf.split(filenames, labels)
-
-    for fold in range(self.n_splits):
-      train_filenames = splited_dataset['X_train'][fold]
-      train_labels = splited_dataset['y_train'][fold]
-      test_filenames = splited_dataset['X_test'][fold]
-      test_labels = splited_dataset['y_test'][fold]
-
-      train_examples = {
-        'image': [self.images_folder / filename for filename in train_filenames],
-        'labels': train_labels
-      }
-
-      test_examples = {
-        'image': [self.image_folder / filename for filename in test_filenames],
-        'labels': test_labels
-      }
-
-      write_tfrecord_file(
-        file_path=self.output / f'train_fold_{fold}.tfrecord',
-        examples=train_examples
-      )
-
-      write_tfrecord_file(
-        file_path=self.output / f'test_fold_{fold}.tfrecord',
-        examples=test_examples
-      )
-
-
 
 class DistributionKFold:
   def __init__(self, bins: int):
     self.bins = bins
 
 
-  def split(self, distribution):
+  def split(self, distribution) -> Generator:
     bin_edges = np.histogram_bin_edges(distribution, bins=self.bins)
 
     for i in range(len(bin_edges) - 1):
@@ -239,10 +40,11 @@ class DistributionKFold:
 
 
 class StratifiedDistributionKFold:
-  def __init__(self, distribution, n_splits: int, bins: int):
+  def __init__(self, distribution, n_splits: int, bins: int, shuffle: bool = True):
     self.n_splits = n_splits
     self.bins = bins
     self.distribution = distribution
+    self.shuffle = shuffle
 
 
   @staticmethod
@@ -272,7 +74,26 @@ class StratifiedDistributionKFold:
     return max_bins
 
 
-  def split(self, X, y) -> dict:
+  def split_ids(self, X, y) -> dict:
+    train = [np.array([], dtype=np.int32) for _ in range(self.n_splits)]
+    test = [np.array([], dtype=np.int32) for _ in range(self.n_splits)]
+
+    dist_kf = DistributionKFold(bins=self.bins)
+    class_kf = StratifiedKFold(n_splits=self.n_splits, shuffle=self.shuffle, random_state=RANDOM_SEED)
+
+    # First split: distribution split
+    for bin_ids in dist_kf.split(distribution=self.distribution):
+      y_bin = y[bin_ids]
+
+      # Second split: class split
+      for i, (train_ids, test_ids) in enumerate(class_kf.split(bin_ids, y_bin)):
+        train[i] = np.concatenate((train[i], bin_ids[train_ids].astype(np.int32)))
+        test[i] = np.concatenate((test[i], bin_ids[test_ids].astype(np.int32)))
+
+    return (train, test)
+
+
+  def split_all(self, X, y) -> dict:
     X_train = [[] for _ in range(self.n_splits)]
     X_test = [[] for _ in range(self.n_splits)]
     y_train = [[] for _ in range(self.n_splits)]
@@ -287,7 +108,7 @@ class StratifiedDistributionKFold:
       y_bin = y[bin_ids]
 
       # Second split: class split
-      for i, (train_ids, test_ids) in class_kf.split(X_bin, y_bin):
+      for i, (train_ids, test_ids) in enumerate(class_kf.split(X_bin, y_bin)):
         X_train[i] = np.concatenate((X_train[i], X_bin[train_ids]))
         X_test[i] = np.concatenate((X_test[i], X_bin[test_ids]))
         y_train[i] = np.concatenate((y_train[i], y_bin[train_ids]))
@@ -307,7 +128,47 @@ class StratifiedDistributionKFold:
 
 
 
-class MergerNetDataset:
+class DatasetConfig:
+  """Configuration params for dataset."""
+  def __init__(
+    self,
+    name: str = None,
+    archive_url: str = None,
+    table_url: str = None,
+    archive_path: Path = None,
+    images_path: Path = None,
+    table_path: Path = None,
+    X_column: str = 'ID',
+    y_column: str = 'class',
+    fold_column: str = 'fold',
+    X_column_suffix: str = '', # filename extension
+    detect_img_extension: bool = False,
+    label_map: dict = None
+  ):
+    self.name = name
+    self.archive_url = archive_url
+    self.table_url = table_url
+    self.archive_path = archive_path
+    self.images_path = images_path
+    self.table_path = table_path
+    self.X_column = X_column
+    self.y_column = y_column
+    self.fold_column = fold_column
+    self.X_column_suffix = X_column_suffix
+    self.detect_img_extension = detect_img_extension
+    self.label_map = label_map
+
+  def __repr__(self) -> str:
+    return (
+      f'Using dataset {self.name.upper()}.\n'
+      f'Archive path:\t\t{str(self.archive_path.resolve())}'
+      f'Images path:\t\t{str(self.images_path.resolve())}'
+      f'Table path:\t\t{str(self.table_path.resolve())}'
+      f'Label map:\t\t{str(self.label_map)}'
+    )
+
+
+class Dataset:
   """High-level representation of dataset.
 
   Parameters
@@ -321,32 +182,39 @@ class MergerNetDataset:
     Configuration object.
   """
   RGB_CONFIG = DatasetConfig(
-    download_url='',
-    save_path=Path('datasets/mergernet_rgb.tar.gz'),
-    train_glob=Path('train.*.tfrecord'),
-    validation_glob=Path('validation.*.tfrecord'),
-    test_glob=Path('test.*.tfrecord')
+    archive_path=Path('datasets/mergernet_rgb.tar.gz'),
+    images_path=Path(''),
+    table_path=Path('')
   )
   """Default configuration object for RGB dataset."""
 
 
-  FITS_CONFIG = DatasetConfig(
-    download_url='',
-    save_path=Path('datasets/mergernet_fits.tar.gz'),
-    train_glob=Path('train.*.tfrecord'),
-    validation_glob=Path('validation.*.tfrecord'),
-    test_glob=Path('test.*.tfrecord')
-  )
-  """Default configuration object for FITS dataset."""
-
-
-  def __init__(self, ds_split: str, ds_type: str = 'rgb'):
+  def __init__(self, data_path: Path, ds_type: str = 'rgb'):
     self.ds_type = ds_type
+    self.data_path = data_path
 
     if ds_type == 'rgb':
-      self.config = MergerNetDataset.RGB_CONFIG
+      self.config = Dataset.RGB_CONFIG
     elif ds_type == 'fits':
-      self.config = MergerNetDataset.FITS_CONFIG
+      self.config = Dataset.FITS_CONFIG
+
+    self.download()
+
+    if self.config.detect_img_extension:
+      self._detect_img_extension()
+
+
+  def _detect_img_extension(self):
+    self.config.X_column_suffix = next(self.config.images_path.iterdir()).suffix
+
+
+  def _discretize_label(self, y: np.ndarray) -> np.ndarray:
+    y_int = np.empty(y.shape, dtype=np.int64)
+
+    for k, v in self.config.label_map:
+      y_int[y == k] = v
+
+    return y_int
 
 
   def download(self) -> None:
@@ -365,12 +233,73 @@ class MergerNetDataset:
     )
 
 
-  def prepare(self) -> None:
-    """TODO: doc"""
-    ds = tf.data.TFRecordDataset(tf.io.gfile.glob(str(self.config.train_glob)))
-    ds = ds.map(parser)
+  def get_fold(self, fold: int) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+    df = pd.read_csv(self.config.table_path)
+
+    df_test = df[df[self.config.fold_column] == fold]
+    df_train = df[df[self.config.fold_column] != fold]
+
+    X_train = df_train[[self.config.X_column]].to_numpy()
+    y_train = df_train[[self.config.y_column]].to_numpy()
+    X_test = df_test[[self.config.X_column]].to_numpy()
+    y_test = df_test[[self.config.y_column]].to_numpy()
+
+    X_train = np.array([
+      str((self.config.images_path / (X + self.config.X_column_suffix)).resolve())
+      for X in X_train
+    ])
+
+    X_test = np.array([
+      str((self.config.images_path / (X + self.config.X_column_suffix)).resolve())
+      for X in X_test
+    ])
+
+    if self.config.label_map:
+      y_train = self._discretize_label(y_train)
+      y_test = self._discretize_label(y_test)
+
+    ds_train = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+    ds_test = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+
+    return ds_train, ds_test
 
 
-  def to_tensorflow(self) -> tf.data.Dataset:
-    """Returns a `tf.data.Dataset` instance."""
-    tf.data.TFRecordDataset()
+  def compute_class_weight(self) -> dict:
+    # TODO: TEST
+    y = pd.read_csv(self.config.table_path)[self.config.y_column].to_numpy()
+    classes, cardinalities = np.unique(y, return_counts=True)
+    total = np.sum(cardinalities)
+    n = len(cardinalities)
+    weight_map = {}
+
+    for class_, cardinality in zip(classes, cardinalities):
+      weight_map[class_] = total / (n * cardinality)
+
+    return weight_map
+
+
+  @staticmethod
+  def concat_fold_column(
+    df: pd.DataFrame,
+    fname_column: Union[str, Sequence] = None,
+    class_column: Union[str, Sequence] = None,
+    r_column: Union[str, Sequence] = None,
+    n_splits: int = 5,
+    bins: int = 3
+  ) -> pd.DataFrame:
+    X = df[fname_column].to_numpy() # filenames
+    y = df[class_column].to_numpy() # class labels
+    r = df[r_column].to_numpy() # r band magnitude
+
+    kf = StratifiedDistributionKFold(distribution=r, n_splits=n_splits, bins=bins)
+    _, test_ids = kf.split_ids(X, y) # using test ids to identify folds
+
+    folds = np.empty(X.shape, dtype=np.int32)
+
+    for i in range(len(test_ids)):
+      folds[test_ids] = i
+
+    df = pd.concat([df, pd.DataFrame({'fold': folds})], axis=1)
+
+    return df
+
