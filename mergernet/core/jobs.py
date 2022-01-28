ac115f9c74b71bdd1fc3e3419b391b72b2cbc073
Natanael <natanael_magalhaes@msn.com>
2022-01-28 11:11:11 -0300

    BaseJob class reimplementation

--- mergernet/core/jobs.py
+++ mergernet/core/jobs.py
@@ -1,39 +1,71 @@
+from enum import Enum
 from pathlib import Path
 from importlib import import_module
+from typing import Sequence, Union
+from datetime import datetime, timezone, timedelta
 import re
 import secrets
+import json
+from mergernet.core.artifacts import ArtifactHelper, JobArtifact
 
-from mergernet.core.logger import Logger
-
-
-
-class Job:
-  id = 1
-
-  def __init__(self):
-    self.run_id = None
-    self.logger = None
+from mergernet.core.constants import GITHUB_PATH, GITHUB_REPO, GITHUB_TOKEN, GITHUB_USER, GDRIVE_PATH
+from mergernet.services.google import GDrive
+from mergernet.services.github import GithubService
 
 
-  def start_execution(self):
-    self.pre_run()
-    self.run()
+class BaseJob:
+  def start_execution(self, data_path: Union[str, Path] = None, **kwargs):
+    self.pre_run(data_path=data_path)
+    self.run(**kwargs)
     self.post_run()
 
 
-  def pre_run(self):
-    self.run_id = secrets.token_hex(nbytes=8)
-    self.logger = Logger()
-    print('pre run')
+  def pre_run(self, data_path: Union[str, Path] = None):
+    # setup storage folders
+    self.runid = secrets.token_hex(3)
+    self.data_path = Path(data_path)
+    self.artifact_path = self.data_path / f'job_{self.jobid}_run_{self.runid}'
+
+    # config artifact helper
+    ah = ArtifactHelper()
+    ah.config(
+      artifact_path=self.artifact_path,
+      gdrive_path=GDRIVE_PATH,
+      use_gdrive=True,
+      use_github=True
+    )
+
+    # create and upload job metadata artifact
+    tz = timezone(timedelta(hours=-3))
+    now = datetime.now(tz=tz).isoformat(sep=' ', timespec='seconds')
+    job_artifact = dict(
+      jobid=self.jobid,
+      runid=self.runid,
+      name=self.name,
+      description=self.description,
+      timestamp=now
+    )
+    ah.save_json(job_artifact, 'job.json')
+    ah.upload('job.json')
 
 
   def run(self):
-    print('empty job')
+    print('Job not implemented')
 
 
   def post_run(self):
-    self.logger.save()
-    print('post run')
+    pass
+    # gh = GithubService(user=GITHUB_USER, token=GITHUB_TOKEN, repo=GITHUB_REPO)
+    # run = gh.get_lastest_job_run(jobid=self.jobid)
+    # if run:
+    #   new_folder = f'job_{self.jobid}_run_{(run + 1):03}'
+    # else:
+    #   new_folder = f'job_{self.jobid}_run_001'
+
+    # if self.data_path:
+    #   self.artifacts_path = self.data_path / new_folder
+    # else:
+    #   self.artifacts_path = Path('/tmp') / new_folder
 
 
   def get_system_resources(self):
