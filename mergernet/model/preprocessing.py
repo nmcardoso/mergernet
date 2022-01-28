68aecf76b582f66f738070b06da603d05886735b
Natanael <natanael_magalhaes@msn.com>
2022-01-28 11:11:11 -0300

    fix functions params and return

--- mergernet/model/preprocessing.py
+++ mergernet/model/preprocessing.py
@@ -1,27 +1,23 @@
-from tarfile import XGLTYPE
-from typing import Path
-import os
-
 import tensorflow as tf
 
 
 
 def one_hot(X, y):
-  return tf.one_hot(y)
+  return X, tf.one_hot(y, 3)
 
 
 
-def load_jpeg(X, *kargs):
+def load_jpg(X, y):
   img_bytes = tf.io.read_file(X)
   img = tf.io.decode_jpeg(img_bytes, channels=3)
-  return img
+  return img, y
 
 
 
-def normalize_rgb(X, *kargs):
-  return X / 255.
+def normalize_rgb(X, y):
+  return X / 255., y
 
 
 
-def standardize_rgb(X, *kargs):
-  return tf.image.per_image_standardization(X)
+def standardize_rgb(X, y):
+  return tf.image.per_image_standardization(X), y
