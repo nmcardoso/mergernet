2ec211b1036bc3f783f193b5a319a422402c4119
Natanael <natanael_magalhaes@msn.com>
2022-01-28 11:11:11 -0300

    update test job

---
+++ mergernet/jobs/j001_test_train.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pandas as pd
+
+from mergernet.core.jobs import BaseJob, JobArtifact
+from mergernet.core.dataset import Dataset
+from mergernet.model.baseline import ConvolutionalClassifier
+
+
+
+class Job(BaseJob):
+  jobid = 1
+  name = 'Test job'
+  description = 'Test job description'
+
+  def run(self):
+    pass
+    # ds = Dataset(data_path=self.data_path)
+
+    # model = ConvolutionalClassifier(ds)
+    # model.train(
+    #   epochs=2,
+    #   optimizer='adam'
+    # )
+
