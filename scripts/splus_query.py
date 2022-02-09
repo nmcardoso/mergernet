bb088d55a964b838f68f96f199def2b99cd930e9
Natanael <natanael_magalhaes@msn.com>
2022-02-08 23:17:19 -0300

    splus query

---
+++ scripts/splus_query.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import sys
+import os
+import shutil
+
+project_root = str(Path(os.path.abspath('')).parent.resolve())
+sys.path.append(project_root)
+
+from mergernet.services.splus import SplusService
+
+
+sql_file = '../data/sql/splus_stripe.sql'
+with open(sql_file) as fp:
+  sql = fp.read()
+
+splus = SplusService()
+splus.batch_query([sql], save_path=['query.csv'])
