d60b7386df1b3b8c7b1cc3c90bdd9837ffbe1d41
Natanael <natanael_magalhaes@msn.com>
2022-02-12 05:30:00 -0300

    debug

--- mergernet/model/study.py
+++ mergernet/model/study.py
@@ -140,6 +140,7 @@ def objective_factory(dataset):
     L.info(f'[TRAIN] Training finished without errors in {t.duration()}.')
 
     ev = model.evaluate(ds_test)
+    print(model.metrics_names)
     idx = model.metrics_names.index('val_accuracy')
     return ev[idx]
   return objective
