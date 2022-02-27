f420e8bbe8c6f196e2abef04ed15889f82efa3a6
Natanael <natanael_magalhaes@msn.com>
2022-02-27 11:11:11 -0300

    splus query

--- scripts/splus_query.py
+++ scripts/splus_query.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import sys
 import os
 import shutil
-
 project_root = str(Path(os.path.abspath('')).parent.resolve())
 sys.path.append(project_root)
 
@@ -14,4 +13,4 @@ with open(sql_file) as fp:
   sql = fp.read()
 
 splus = SplusService()
-splus.batch_query([sql], save_path=['query.csv'])
+splus.batch_query([sql], save_path=['../data/splus_south_gal80.csv'], scope='private')
