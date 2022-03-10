1fd01c98306770418ba2c12c944acb993478fb5d
Natanael <natanael_magalhaes@msn.com>
2022-03-10 03:57:02 -0300

    make conv arch tunable

--- mergernet/model/study.py
+++ mergernet/model/study.py
@@ -76,8 +76,6 @@ class HyperModel:
     ds_train = ds_train.map(one_hot_factory(self.dataset.config.n_classes))
     ds_test = ds_test.map(one_hot_factory(self.dataset.config.n_classes))
     L.info('[DATASET] apply: one_hot')
-    _x, _y = next(ds_train.take(1).as_numpy_iterator())
-    print('X.shape =', _x.shape, 'y.shape =', _y.shape)
 
     ds_train = ds_train.cache()
     ds_test = ds_test.cache()
@@ -89,8 +87,6 @@ class HyperModel:
     ds_train = ds_train.batch(64)
     ds_test = ds_test.batch(64)
     L.info('[DATASET] apply: batch')
-    _x, _y = next(ds_train.take(1).as_numpy_iterator())
-    print('X.shape =', _x.shape, 'y.shape =', _y.shape)
 
     ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
     ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
@@ -110,13 +106,14 @@ class HyperModel:
   ):
     self.hp.set_trial(trial)
 
-    conv_block = tf.keras.applications.ResNet50(
+    conv_arch, preprocess_input = self._architecture_switch(
+      self.hp.architecture.suggest()
+    )
+    conv_block = conv_arch(
       input_shape=input_shape,
       include_top=False,
       weights=self.hp.pretrained_weights.suggest()
     )
-
-    preprocess_input = tf.keras.applications.resnet.preprocess_input
 
     data_aug_layers = [
       tf.keras.layers.RandomFlip(mode='horizontal', seed=42),
@@ -261,3 +258,64 @@ class HyperModel:
     # path = str((ah.artifact_path / 'optimization_history.png').resolve())
     # ax.figure.savefig(path,  pad_inches=0.01, bbox_inches='tight')
     # ah.upload(fname='optimization_history.png', github=True, gdrive=True)
+
+
+  def _architecture_switch(self, pretrained_arch):
+    if pretrained_arch == 'xception':
+      preprocess_input = tf.keras.applications.xception.preprocess_input
+      base_model = tf.keras.applications.Xception
+    elif pretrained_arch == 'vgg16':
+      preprocess_input = tf.keras.applications.vgg16.preprocess_input
+      base_model = tf.keras.applications.VGG16
+    elif pretrained_arch == 'vgg19':
+      preprocess_input = tf.keras.applications.vgg19.preprocess_input
+      base_model = tf.keras.applications.VGG19
+    elif pretrained_arch == 'resnet50':
+      preprocess_input = tf.keras.applications.resnet.preprocess_input
+      base_model = tf.keras.applications.ResNet50
+    elif pretrained_arch == 'resnet101':
+      preprocess_input = tf.keras.applications.resnet.preprocess_input
+      base_model = tf.keras.applications.ResNet101
+    elif pretrained_arch == 'resnet50v2':
+      preprocess_input = tf.keras.applications.resnet_v2.preprocess_input
+      base_model = tf.keras.applications.ResNet50V2
+    elif pretrained_arch == 'resnet101v2':
+      preprocess_input = tf.keras.applications.resnet_v2.preprocess_input
+      base_model = tf.keras.applications.ResNet101V2
+    elif pretrained_arch == 'inceptionv3':
+      preprocess_input = tf.keras.applications.inception_v3.preprocess_input
+      base_model = tf.keras.applications.InceptionV3
+    elif pretrained_arch == 'inceptionresnetv2':
+      preprocess_input = tf.keras.applications.inception_resnet_v2.preprocess_input
+      base_model = tf.keras.applications.InceptionResNetV2
+    elif pretrained_arch == 'densenet169':
+      preprocess_input = tf.keras.applications.densenet.preprocess_input
+      base_model = tf.keras.applications.DenseNet169
+    elif pretrained_arch == 'densenet201':
+      preprocess_input = tf.keras.applications.densenet.preprocess_input
+      base_model = tf.keras.applications.DenseNet201
+    elif pretrained_arch == 'efficientnetb0':
+      preprocess_input = tf.keras.applications.efficientnet.preprocess_input
+      base_model = tf.keras.applications.EfficientNetB0
+    elif pretrained_arch == 'efficientnetb1':
+      preprocess_input = tf.keras.applications.efficientnet.preprocess_input
+      base_model = tf.keras.applications.EfficientNetB1
+    elif pretrained_arch == 'efficientnetb2':
+      preprocess_input = tf.keras.applications.efficientnet.preprocess_input
+      base_model = tf.keras.applications.EfficientNetB2
+    elif pretrained_arch == 'efficientnetb3':
+      preprocess_input = tf.keras.applications.efficientnet.preprocess_input
+      base_model = tf.keras.applications.EfficientNetB3
+    elif pretrained_arch == 'efficientnetb4':
+      preprocess_input = tf.keras.applications.efficientnet.preprocess_input
+      base_model = tf.keras.applications.EfficientNetB4
+    elif pretrained_arch == 'efficientnetb5':
+      preprocess_input = tf.keras.applications.efficientnet.preprocess_input
+      base_model = tf.keras.applications.EfficientNetB5
+    elif pretrained_arch == 'efficientnetb6':
+      preprocess_input = tf.keras.applications.efficientnet.preprocess_input
+      base_model = tf.keras.applications.EfficientNetB6
+    elif pretrained_arch == 'efficientnetb7':
+      preprocess_input = tf.keras.applications.efficientnet.preprocess_input
+      base_model = tf.keras.applications.EfficientNetB7
+    return base_model, preprocess_input
