f83afb3be26d109d8166773faea79664e824671e
Natanael <natanael_magalhaes@msn.com>
2022-03-11 11:33:10 -0300

    update links

--- scripts/download_splus_stamps.py
+++ scripts/download_splus_stamps.py
@@ -10,35 +10,35 @@ import numpy as np
 from mergernet.services.splus import SplusService, ImageType
 
 
-df = pd.read_csv('../data/splus_south_gal80.csv')
+df = pd.read_csv('../data/blind_splus_gal80_r17.csv')
 
 
 ra = df['RA'].to_list()
 dec = df['DEC'].to_list()
-save_path_1 = [f'../data/splus_trilogy_128/{_id}.png' for _id in df['ID']]
-save_path_2 = [f'../data/splus_lupton_150/{_id}.png' for _id in df['ID']]
-save_path_3 = [f'../data/splus_trilogy_150/{_id}.png' for _id in df['ID']]
+save_path_1 = [f'../data/images/blind_splus_trilogy_128/{_id}.png' for _id in df['ID']]
+save_path_2 = [f'../data/images/blind_splus_lupton_150/{_id}.png' for _id in df['ID']]
+save_path_3 = [f'../data/images/blind_splus_trilogy_150/{_id}.png' for _id in df['ID']]
 
 splus = SplusService()
-splus.batch_image_download(
-  ra=ra,
-  dec=dec,
-  save_path=save_path_1,
-  img_type=ImageType.trilogy,
-  replace=False,
-  size=128,
-  workers=3
-)
+# splus.batch_image_download(
+#   ra=ra,
+#   dec=dec,
+#   save_path=save_path_1,
+#   img_type=ImageType.trilogy,
+#   replace=False,
+#   size=128,
+#   workers=3
+# )
 
-splus.batch_image_download(
-  ra=ra,
-  dec=dec,
-  save_path=save_path_2,
-  img_type=ImageType.lupton,
-  replace=False,
-  size=150,
-  workers=5
-)
+# splus.batch_image_download(
+#   ra=ra,
+#   dec=dec,
+#   save_path=save_path_2,
+#   img_type=ImageType.lupton,
+#   replace=False,
+#   size=150,
+#   workers=5
+# )
 
 splus.batch_image_download(
   ra=ra,
@@ -47,5 +47,5 @@ splus.batch_image_download(
   img_type=ImageType.trilogy,
   replace=False,
   size=150,
-  workers=3
+  workers=8
 )
