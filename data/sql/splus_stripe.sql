0f61c75ca3ba2c59302cc2389c806ad7fb96c0f5
Natanael <natanael_magalhaes@msn.com>
2022-03-14 11:11:11 -0300

    add inferior limit to r_mag and model_flag zero

--- data/sql/splus_stripe.sql
+++ data/sql/splus_stripe.sql
@@ -15,4 +15,6 @@
 SELECT p.ID, p.RA, p.DEC, p.r_auto, model.PROB_GAL
 FROM idr3.r_band AS p
 INNER JOIN idr3_vacs.star_galaxy_quasar AS model ON p.ID = model.ID
-WHERE (p.Field NOT LIKE 'STRIPE%') AND (model.PROB_GAL > 0.8) AND (p.r_auto < 17)
+WHERE (p.Field NOT LIKE 'STRIPE%') AND (model.PROB_GAL > 0.8)
+  AND (p.r_auto > 13.5) AND (p.r_auto < 17) AND (p.e_r_auto < 0.1)
+  AND (model.model_flag = 0) -- AND (p.PhotoFlag_r = 0)
