afedcf13d775510bc83ee1f216ee6cd534a14f2c
Natanael <natanael_magalhaes@msn.com>
2022-03-31 11:11:11 -0300

    updates

--- scripts/download_splus_stamps.py
+++ scripts/download_splus_stamps.py
@@ -8,27 +8,37 @@ sys.path.append(project_root)
 import pandas as pd
 import numpy as np
 from mergernet.services.splus import SplusService, ImageType
+from mergernet.core.constants import SPLUS_BANDS
 
 
-df = pd.read_csv('../data/blind_splus_gal80_r17.csv')
+df = pd.read_csv('../data/blind_splus_gal80_r17_mflag0.csv')
 
 
 ra = df['RA'].to_list()
 dec = df['DEC'].to_list()
-save_path_1 = [f'../data/images/blind_splus_trilogy_128/{_id}.png' for _id in df['ID']]
-save_path_2 = [f'../data/images/blind_splus_lupton_150/{_id}.png' for _id in df['ID']]
-save_path_3 = [f'../data/images/blind_splus_trilogy_150/{_id}.png' for _id in df['ID']]
+save_paths = {
+  band: [
+    f'../data/images/blind_splus_fits_128/{band}/{_id}.fits'
+    for _id in df['ID']
+  ] for band in SPLUS_BANDS
+}
+# save_path_1 = [f'../data/images/blind_splus_trilogy_128/{_id}.png' for _id in df['ID']]
+# save_path_2 = [f'../data/images/blind_splus_lupton_150/{_id}.png' for _id in df['ID']]
+# save_path_3 = [f'../data/images/blind_splus_trilogy_150/{_id}.png' for _id in df['ID']]
 
 splus = SplusService()
-# splus.batch_image_download(
-#   ra=ra,
-#   dec=dec,
-#   save_path=save_path_1,
-#   img_type=ImageType.trilogy,
-#   replace=False,
-#   size=128,
-#   workers=3
-# )
+for i, band in enumerate(SPLUS_BANDS):
+  print(f'[{i+1}/{len(SPLUS_BANDS)}] Downloading band: {band}')
+  splus.batch_image_download(
+    ra=ra,
+    dec=dec,
+    save_path=save_paths[band],
+    img_type=ImageType.fits,
+    replace=False,
+    size=128,
+    workers=18,
+    band=band
+  )
 
 # splus.batch_image_download(
 #   ra=ra,
@@ -40,12 +50,12 @@ splus = SplusService()
 #   workers=5
 # )
 
-splus.batch_image_download(
-  ra=ra,
-  dec=dec,
-  save_path=save_path_3,
-  img_type=ImageType.trilogy,
-  replace=False,
-  size=150,
-  workers=8
-)
+# splus.batch_image_download(
+#   ra=ra,
+#   dec=dec,
+#   save_path=save_path_3,
+#   img_type=ImageType.trilogy,
+#   replace=False,
+#   size=150,
+#   workers=8
+# )
