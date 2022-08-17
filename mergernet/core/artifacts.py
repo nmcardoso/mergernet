ecb4e396be919b943475499fa7710987567e5fd1
Natanael <natanael_magalhaes@msn.com>
2022-08-17 10:11:12 -0300

    rework artifacts module

--- mergernet/core/artifacts.py
+++ mergernet/core/artifacts.py
@@ -7,7 +7,8 @@ import json
 
 import tensorflow as tf
 
-from mergernet.core.constants import GITHUB_BRANCH, GITHUB_USER, GITHUB_TOKEN, GITHUB_REPO, GITHUB_PATH, GDRIVE_PATH
+from mergernet.core.constants import GDRIVE_ARTIFACT_PATH, GH_BRANCH, GH_USER, GH_TOKEN, GH_REPO, GH_BASE_PATH
+from .job import JobInfo
 from mergernet.core.utils import SingletonMeta
 from mergernet.services.github import GithubService
 from mergernet.services.google import GDrive
@@ -16,77 +17,56 @@ from mergernet.services.google import GDrive
 L = logging.getLogger('job')
 
 
-class ArtifactHelper(metaclass=SingletonMeta):
-  use_github: bool = False
-  use_gdrive: bool = True
-  artifact_path: Union[Path, None] = None
-  gdrive_path: Union[Path, None] = None
+class ArtifactHelper:
+  @staticmethod
+  def upload_model(model: tf.keras.Model):
+    path = GDRIVE_ARTIFACT_PATH.format(
+      job_id=JobInfo.job_id,
+      run_id=JobInfo.run_id,
+      artifact_path=f'{model.name}.h5'
+    )
+    model.save(path)
 
 
-  def config(
-    self,
-    artifact_path: Union[str, Path] = None,
-    gdrive_path: Union[str, Path] = None,
-    use_github: bool = None,
-    use_gdrive: bool = None,
-    use_optuna: bool = True
-  ):
-    # TODO: always generate artifact path
-    if artifact_path:
-      self.artifact_path = Path(artifact_path)
-      if not self.artifact_path.exists():
-        self.artifact_path.mkdir(parents=True, exist_ok=True)
-
-    if gdrive_path:
-      self.gdrive_path = Path(gdrive_path)
-      # TODO: check if this path exists (is_mounted() == True)
-
-    if use_github is not None: self.use_github = use_github
-    if use_gdrive is not None: self.use_gdrive = use_gdrive
-
-    self.github = GithubService(user=GITHUB_USER, token=GITHUB_TOKEN, repo=GITHUB_REPO)
-    self.gdrive = GDrive(base_path=gdrive_path)
-
-    if use_optuna:
-      (self.artifact_path / 'optuna').mkdir(exist_ok=True)
-
-
-  def upload(
-    self,
-    fname: str = None,
-    path: Union[str, Path] = None,
-    github: bool = None,
-    gdrive: bool = None
-  ):
-    use_github = github if github is not None else self.use_github
-    use_gdrive = gdrive if gdrive is not None else self.use_gdrive
-    path = Path(path) if path else self.artifact_path / fname
-
-    suffix = path.suffix
-    github_ext = ['.json', '.log', '.txt', '.csv', '.png', '.svg', '.pdf', '.jpg', '.jpeg']
-
-    if use_github and suffix in github_ext:
-      self._upload_github(path)
-
-    if use_gdrive:
-      self._upload_gdrive(path)
-
-
-  def upload_log(self, name: str):
-    self.upload(path=f'/tmp/{name}.log', github=True, gdrive=True)
-
-
-  def upload_model(self, model: tf.keras.Model):
-    filename = f'{model.name}.h5'
-    path = self.artifact_path / filename
-    model.save(str(path))
-    self.upload(path=path, github=False, gdrive=True)
 
     summary_fname = f'{model.name}.txt'
     summary_path = self.artifact_path / summary_fname
     with open(summary_path, 'w') as fp:
       model.summary(print_fn=lambda x: fp.write(x + '\n'))
     self.upload(path=summary_path, github=True, gdrive=True)
+
+
+  def upload_optuna_db(self):
+    name = self.optuna_db_name
+
+    if name is None:
+      raise ValueError("Can't resolve optuna db name. Set ah.optuna_db_name or arg.")
+
+    name = str(Path(name).stem)
+
+    local_path = self.artifact_path / 'optuna' / f'{name}.sqlite'
+    remote_path = f'optuna/{name}.sqlite'
+
+    with open(local_path, 'rb') as fp:
+      self.github.commit(path=remote_path, data=fp, from_bytes=True)
+
+
+  def download_optuna_db(self):
+    name = self.optuna_db_name
+
+    if name is None:
+      raise ValueError("Can't resolve optuna db name. Set ah.optuna_db_name or arg.")
+
+    name = str(Path(name).stem)
+
+    self.github.download(
+      remote_path=f'optuna/{name}.sqlite',
+      dest_path=self.artifact_path / 'optuna' / f'{name}.sqlite'
+    )
+
+
+  def upload_log(self, name: str):
+    self.upload(path=f'/tmp/{name}.log', github=True, gdrive=True)
 
 
   def upload_json(self, data: object, filename: Union[str, Path]):
@@ -110,61 +90,14 @@ class ArtifactHelper(metaclass=SingletonMeta):
     self._upload_gdrive(path)
 
 
-  def upload_optuna_db(self, name: str):
-    if name is None:
-      name = self.optuna_db_name
-
-    if name is None:
-      raise ValueError("Can't resolve optuna db name. Set ah.optuna_db_name or arg.")
-
-    name = str(Path(name).stem)
-
-    local_path = self.artifact_path / 'optuna' / f'{name}.sqlite'
-    remote_path = f'optuna/{name}.sqlite'
-
-    if self.gdrive.exists(remote_path):
-      tz = timezone(timedelta(hours=-3))
-      d = datetime.now(tz)
-      ts = '{}-{:02d}-{:02d}_{:02d}-{:02d}-{:02d}'.format(
-        d.year, d.month, d.day, d.hour, d.minute, d.second
-      )
-      backup_path = f'optuna/backup/{name}_{ts}.sqlite'
-      self.gdrive.move(remote_path, backup_path)
-      L.info(f'[GDRIVE] created optuna backup at {backup_path}.')
-
-    if self.gdrive.send(local_path, remote_path):
-      L.info(f'[GDRIVE] uploaded optuna database {name}.sqlite.')
-    else:
-      L.info(f'[GDRIVE] an error occurred during optuna dataset upload.')
-
-
-  def download_optuna_db(self, name: str = None):
-    if name is None:
-      name = self.optuna_db_name
-
-    if name is None:
-      raise ValueError("Can't resolve optuna db name. Set ah.optuna_db_name or arg.")
-
-    name = str(Path(name).stem)
-    remote_path = Path(f'optuna/{name}.sqlite')
-
-    if remote_path.exists():
-      if self.gdrive.get(remote_path, self.artifact_path / 'optuna' / f'{name}.sqlite'):
-        L.info(f'[GDRIVE] optuna/{name}.sqlite successfully downloaded.')
-      else:
-        L.info(f'[GDRIVE] error downloading optuna/{name}.sqlite.')
-    else:
-      L.info(f'[GDRIVE] optuna/{name}.sqlite was not found in google drive, download skiped.')
-
-
   def _upload_github(self, path: Union[str, Path]):
     path = Path(path)
     filename = str(path.name)
     with open(path, 'rb') as file:
       file_bytes = file.read()
 
-    gh_path = f'{GITHUB_PATH}/{self.artifact_path.stem}/{filename}'
-    if self.github.commit(gh_path, file_bytes, GITHUB_BRANCH, from_bytes=True):
+    gh_path = f'{GH_BASE_PATH}/{self.artifact_path.stem}/{filename}'
+    if self.github.commit(gh_path, file_bytes, GH_BRANCH, from_bytes=True):
       L.info(f'[GITHUB] file "{filename}" successfully uploaded to github.')
     else:
       L.error(f'[GITHUB] unable to upload the file "{filename}"')
