0d0d6b3189764c1927d38d0110b26b474446816a
Natanael <natanael_magalhaes@msn.com>
2022-09-02 08:09:10 -0300

    separate DatasetConfig and DatasetRegistry to another module

---
+++ mergernet/data/dataset_config.py
@@ -0,0 +1,207 @@
+from pathlib import Path
+from typing import Dict, Union
+
+from mergernet.services.google import GDrive
+from mergernet.services.sciserver import SciServer
+
+
+
+class DatasetConfig:
+  """Configuration params for dataset."""
+  def __init__(
+    self,
+    name: str = None,
+    archive_url: Union[str, Dict[str, str]] = None,
+    table_url: Union[str, Dict[str, str]] = None,
+    archive_path: Path = None,
+    images_path: Path = None,
+    table_path: Path = None,
+    X_column: str = 'ID',
+    y_column: str = 'class',
+    fold_column: str = 'fold',
+    X_column_suffix: str = '', # filename extension
+    detect_img_extension: bool = False,
+    label_map: dict = None,
+    image_shape: tuple = None,
+    n_classes: int = None
+  ):
+    self.name = name
+    self.archive_url = archive_url
+    self.table_url = table_url
+    self.archive_path = archive_path
+    self.images_path = images_path
+    self.table_path = table_path
+    self.X_column = X_column
+    self.y_column = y_column
+    self.fold_column = fold_column
+    self.X_column_suffix = X_column_suffix
+    self.detect_img_extension = detect_img_extension
+    self.label_map = label_map
+    self.image_shape = image_shape
+    self.n_classes = n_classes
+
+
+
+DARG_NO_INSPECTION = DatasetConfig(
+  # table_url=GDrive.get_url('1yHnyOdXS-HKzIsbenSi646jyf2AWU9vo'),
+  name='darg_no_inspection',
+  archive_url=GDrive.get_url('1ltKXhZgA4Ab60FGKCqybhebSiAL2LMgG'),
+  table_url=GDrive.get_url('1QgUYkzcjaCmo-kcgM8s2U8PYlx0C58oD'),
+  archive_path=Path('sdss_lupton_jpg_128.tar.xz'),
+  images_path=Path('sdss_lupton_jpg_128'),
+  table_path=Path('reference_darg.csv'),
+  X_column='filename',
+  y_column='class',
+  fold_column='fold',
+  label_map={ 'E': 0, 'M': 1, 'S': 2 },
+  image_shape=(128, 128, 3),
+  n_classes=3
+)
+"""Default configuration object for RGB dataset."""
+
+MESD_SDSS_128 = DatasetConfig(
+  name='mesd_sdss_128',
+  archive_url=GDrive.get_url('1YZ7A9rVglf5NJp27rW8-6aJXBfBNPfYv'),
+  table_url=GDrive.get_url('1uaRXWUskBrLHi-IGLhZ5T6yIa8w5hi4H'),
+  archive_path=Path('mesd_sdss_128.tar.xz'),
+  images_path=Path('mesd_sdss_128'),
+  table_path=Path('mesd.csv'),
+  X_column='iauname',
+  X_column_suffix='.jpg',
+  y_column='class',
+  fold_column='fold',
+  label_map={'merger': 0, 'elliptical': 1, 'spiral': 2, 'disturbed': 3},
+  image_shape=(128, 128, 3),
+  n_classes=4
+)
+"""MESD dataset with SDSS 128x128 images."""
+
+MESD_LEGACY_128 = DatasetConfig(
+  name='mesd_legacy_128',
+  archive_url=GDrive.get_url('1cTU0SVEv3qVeVxF7pzhtOP7S-bx5cPOP'),
+  table_url=GDrive.get_url('1uaRXWUskBrLHi-IGLhZ5T6yIa8w5hi4H'),
+  archive_path=Path('mesd_legacy_128.tar.xz'),
+  images_path=Path('mesd_legacy_128'),
+  table_path=Path('mesd.csv'),
+  X_column='iauname',
+  X_column_suffix='.jpg',
+  y_column='class',
+  fold_column='fold',
+  label_map={'merger': 0, 'elliptical': 1, 'spiral': 2, 'disturbed': 3},
+  image_shape=(128, 128, 3),
+  n_classes=4
+)
+"""MESD dataset with Legacy Survey 128x128 images."""
+
+BIN_SDSS_128 = DatasetConfig(
+  name='bin_sdss_128',
+  archive_url=GDrive.get_url('1pZqGs6xG12Od-g3rnXEOyJ40LPAPu0J2'),
+  table_url=GDrive.get_url('1N-o1N3dYjJRaU4Nu9ZC0j2OHXzhbwYgl'),
+  archive_path=Path('bin_sdss_128.tar.xz'),
+  images_path=Path('bin_sdss_128'),
+  table_path=Path('bin_sdss.csv'),
+  X_column='iauname',
+  X_column_suffix='.jpg',
+  y_column='class',
+  fold_column='fold',
+  label_map={'non_merger': 0, 'merger': 1},
+  image_shape=(128, 128, 3),
+  n_classes=2
+)
+"""Binary dataset (merger and non-merger) with SDSS 128x128 images."""
+
+BLIND_SPLUS_LUPTON_128 = DatasetConfig(
+  name='blind_splus_lupton_128',
+  archive_url={
+    'gdrive': GDrive.get_url('1TktT_u_NTqyBPKq5KG8hW0TVYhZe6Q3r'),
+    'sciserver': SciServer.get_url('28d96731-35d9-4eac-bdee-7ccb81c5456d')
+  },
+  table_url=GDrive.get_url('1qd0lxMf2WzPF8bW0NciX1wwfa_mrS0Om'),
+  archive_path=Path('blind_splus_lupton_128.tar.xz'),
+  images_path=Path('blind_splus_lupton_128'),
+  table_path=Path('blind_splus_gal80_r17_lupton.csv'),
+  X_column='ID',
+  X_column_suffix='.png',
+  image_shape=(128, 128, 3)
+)
+"""Blind dataset with S-PLUS 128x128 Lupton images."""
+
+BLIND_SPLUS_LUPTON_150 = DatasetConfig(
+  name='blind_splus_lupton_150',
+  archive_url={
+    'gdrive': GDrive.get_url('1qERSsv9W4d2ICyKlvhhPaR7a2j6W5HrD'),
+    'sciserver': SciServer.get_url('946b0d5c-741e-463a-bdbc-4a04313c00c7')
+  },
+  table_url=GDrive.get_url('1qd0lxMf2WzPF8bW0NciX1wwfa_mrS0Om'),
+  archive_path=Path('blind_splus_lupton_150.tar.xz'),
+  images_path=Path('blind_splus_lupton_150'),
+  table_path=Path('blind_splus_gal80_r17_lupton.csv'),
+  X_column='ID',
+  X_column_suffix='.png',
+  image_shape=(150, 150, 3)
+)
+"""Blind dataset with S-PLUS 150x150 Lupton images."""
+
+BLIND_SPLUS_TRILOGY_128 = DatasetConfig(
+  name='blind_splus_trilogy_128',
+  archive_url={
+    'gdrive': GDrive.get_url('1lPvOtE6HCJ7Xi5hAGfjgvrfwGiLRT7uH'),
+    'sciserver': SciServer.get_url('4fe38e2a-c1db-4cd3-92ec-1c2c5b2c5284')
+  },
+  table_url=GDrive.get_url('1JEjQleflgQf_L0Qkun15PHUn-OnOG0rG'),
+  archive_path=Path('blind_splus_trilogy_128.tar.xz'),
+  images_path=Path('blind_splus_trilogy_128'),
+  table_path=Path('blind_splus_gal80_r17_trilogy.csv'),
+  X_column='ID',
+  X_column_suffix='.png',
+  image_shape=(128, 128, 3)
+)
+"""Blind dataset with S-PLUS 128x128 Trilogy images."""
+
+BLIND_SPLUS_TRILOGY_150 = DatasetConfig(
+  name='blind_splus_trilogy_150',
+  archive_url={
+    'gdrive': GDrive.get_url('1aiLtmbrJSRQTmSB3XP1AqXi0tfUi3S-H'),
+    'sciserver': SciServer.get_url('74ca0cba-6c9e-4022-95b8-9d7964d70947'),
+  },
+  table_url=GDrive.get_url('1JEjQleflgQf_L0Qkun15PHUn-OnOG0rG'),
+  archive_path=Path('blind_splus_trilogy_150.tar.xz'),
+  images_path=Path('blind_splus_trilogy_150'),
+  table_path=Path('blind_splus_gal80_r17_trilogy.csv'),
+  X_column='ID',
+  X_column_suffix='.png',
+  image_shape=(150, 150, 3)
+)
+"""Blind dataset with S-PLUS 150x150 Trilogy images."""
+
+BIN_LEGACY_NORTH_RGB_128 = DatasetConfig(
+  name='bin_legacy_north_rgb_128',
+  archive_url={
+    'sciserver': SciServer.get_url('f0332ec2-a3c8-4d62-9a19-5d1e4063a7ed'),
+    'gdrive': GDrive.get_url('1eyumx1QCfXVTzPtdaKT67REl3XEo03eS')
+  },
+  table_url=GDrive.get_url('1oybZ9zcRX8670bDtpRmI3j_VzYY30ldI'),
+  archive_path=Path('bin_legacy_north_rgb_128.tar.xz'),
+  images_path=Path('bin_legacy_north_rgb_128'),
+  table_path=Path('bin_legacy_north.csv'),
+  X_column='iauname',
+  X_column_suffix='.jpg',
+  image_shape=(128, 128, 3)
+)
+"""Binary dataset (merger and non-merger) with Legacy 128x128 RGB images on north sky."""
+
+
+
+class DatasetRegistry:
+  """
+  A registry for all datasets
+  """
+  DARG_NO_INSPECTION = DARG_NO_INSPECTION
+  MESD_SDSS_128 = MESD_SDSS_128
+  MESD_LEGACY_128 = MESD_LEGACY_128
+  BIN_SDSS_128 = BIN_SDSS_128
+  BLIND_SPLUS_LUPTON_128 = BLIND_SPLUS_LUPTON_128
+  BLIND_SPLUS_LUPTON_150 = BLIND_SPLUS_LUPTON_150
+  BLIND_SPLUS_TRILOGY_128 = BLIND_SPLUS_TRILOGY_128
+  BLIND_SPLUS_TRILOGY_150 = BLIND_SPLUS_TRILOGY_150
+  BIN_LEGACY_NORTH_RGB_128 = BIN_LEGACY_NORTH_RGB_128
