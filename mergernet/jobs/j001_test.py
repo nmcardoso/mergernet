36659f4a2d7a7db553739130646ef7f08f6cf0a5
Natanael <natanael_magalhaes@msn.com>
2022-09-05 18:48:07 -0300

    fix epoch number

--- mergernet/jobs/j001_test.py
+++ mergernet/jobs/j001_test.py
@@ -7,7 +7,7 @@ from mergernet.model.baseline import finetune_train
 hps = HyperParameterSet(
   HP.const('architecture', 'resnet50'),
   HP.const('pretrained_weights', 'imagenet'),
-  HP.const('epochs', 1),
+  HP.const('epochs', 12),
   HP.const('batch_size', 64),
   HP.num('dense_1_units', low=64, high=1024, step=64, dtype=int),
   HP.num('dropout_1_rate', low=0.2, high=0.5),
