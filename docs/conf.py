8997f43c6e10714660a61faa6a47b465225d509f
Natanael <natanael_magalhaes@msn.com>
2022-12-13 12:12:12 -0300

    fix title

--- docs/conf.py
+++ docs/conf.py
@@ -175,7 +175,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-  (master_doc, 'mergernet', 'mergernet Documentation',
+  (master_doc, 'mergernet', 'Mergernet Documentation',
     [author], 1)
 ]
 
