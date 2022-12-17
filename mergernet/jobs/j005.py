17ee96088dcc8694cb4cc55520c9f250b445b0e6
Natanael <natanael_magalhaes@msn.com>
2022-12-17 16:21:58 -0300

    experiment 05

---
+++ mergernet/jobs/j005.py
@@ -0,0 +1,38 @@
+from mergernet.core.experiment import Experiment, backup_model
+from mergernet.core.hp import HP, HyperParameterSet
+from mergernet.data.dataset import Dataset
+from mergernet.model.automl import optuna_train
+from mergernet.model.baseline import finetune_train
+
+
+class Job(Experiment):
+  def __init__(self):
+    self.exp_id = 5
+    self.log_wandb = True
+
+  def call(self):
+    hps = HyperParameterSet(
+      HP.const('architecture', 'resnet50'),
+      HP.const('pretrained_weights', 'imagenet'),
+      HP.const('epochs', 34),
+      HP.const('batch_size', 64),
+      HP.num('dense_1_units', low=64, high=1024, step=64, dtype=int),
+      HP.num('dropout_1_rate', low=0.2, high=0.5),
+      HP.num('dense_2_units', low=64, high=1024, step=64, dtype=int),
+      HP.num('dropout_2_rate', low=0.2, high=0.5),
+      HP.num('opt_lr', low=1e-5, high=1e-3, log=True)
+    )
+
+    ds = Dataset(config=Dataset.registry.BIN_LEGACY_NORTH_RGB_128)
+    model = optuna_train(
+      train_func=finetune_train,
+      dataset=ds,
+      hp=hps,
+      n_trials=12
+    )
+    self.upload_file_gd('model.h5', model)
+
+
+if __name__ == '__main__':
+  j = Job()
+  j.run()
