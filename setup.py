f6c3a4cfcbe2b1f75dcfb06c7682703c4b4fadc7
Natanael <natanael_magalhaes@msn.com>
2022-12-27 00:44:28 -0300

    :package: update dependencies versions

--- setup.py
+++ setup.py
@@ -13,24 +13,24 @@ setup(
   },
   install_requires=[
     'wheel',
-    'numpy>=1.19.5',
-    'pandas>=1.1.5',
-    'tensorflow>=2.7',
-    'scikit-learn>=1.0.1',
-    'tqdm>=4.62.3',
+    'numpy>=1.21.6',
+    'pandas>=1.3.5',
+    'tensorflow>=2.9.2',
+    'scikit-learn>=1.0.2',
+    'tqdm>=4.64.1',
     'requests>=2.23',
     'Pillow>=7.1.2',
-    'astropy>=4.3.1'
+    'astropy>=5.2'
     'tensorflow_addons',
     'tensorboard',
     'optuna',
     'plotly',
     'wandb',
+    'zoobot==0.0.4',
     'keras_applications', # zoobot
     'tensorflow_probability>=0.11', # zoobot
     'seaborn' , # zoobot
-    'zoobot',
-    'pydot', # tf.keras.utils.plot_model
+    'pydot>=1.4.2', # tf.keras.utils.plot_model
   ],
   extras_require={
     'docs': [
