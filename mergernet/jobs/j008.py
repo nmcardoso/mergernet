62e3c99b8119612632a7994ee9ee19d5f957000a
Natanael <natanael_magalhaes@msn.com>
2023-02-03 00:32:34 -0300

    use new compression function

--- mergernet/jobs/j008.py
+++ mergernet/jobs/j008.py
@@ -1,9 +1,9 @@
+import numpy as np
 import pandas as pd
 
 from mergernet.core.constants import DATA_ROOT
 from mergernet.core.experiment import Experiment
-from mergernet.core.utils import (compress_images, iauname,
-                                  iauname_relative_path)
+from mergernet.core.utils import compress_files, iauname, iauname_relative_path
 from mergernet.services.legacy import LegacyService
 
 
@@ -16,17 +16,21 @@ class Job(Experiment):
 
 
   def call(self):
-    path = DATA_ROOT / 'images' / 'decals_0.364_png'
-    output_path = DATA_ROOT / 'images' / 'decals_0.364_tar' / 'decals_0.364.tar.xz'
-    iaunames = [i.stem for i in path.glob('**/*.png')]
+    images_path = DATA_ROOT / 'images' / 'decals_0.364_png'
+    output_folder = DATA_ROOT / 'images' / 'decals_0.364_tar'
+    all_folders = [p.name for p in sorted(images_path.glob('*')) if p.is_dir()]
+    folders_per_part = 10
+    parts = int(np.ceil(len(all_folders) / folders_per_part))
 
-    compress_images(
-      iaunames=iaunames,
-      base_path=path,
-      image_ext='png',
-      output_path=output_path,
-      max_files=2
-    )
+    for part in range(parts):
+      print(f'>> Part {part + 1} of {parts}')
+      folders = ','.join(all_folders[part*folders_per_part : (part+1)*folders_per_part-1])
+      compress_files(
+        input_path=images_path / f'{{{folders}}}',
+        output_path=output_folder / f'decals_0.364_part{part}.tar.xz',
+        level=0,
+      )
+      print()
 
 
 
