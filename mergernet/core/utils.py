577800a2476461e93014426571e07a815784a5f8
Natanael <natanael_magalhaes@msn.com>
2023-02-04 18:13:33 -0300

    fix `extract_files` destination path

--- mergernet/core/utils.py
+++ mergernet/core/utils.py
@@ -210,6 +210,12 @@ def extract_files(archive: Union[str, Path], dest: Union[str, Path]):
 
   command = f'pv {str(archive.resolve())} | tar -xJ -C {str(dest.resolve())}'
 
+  if dest.is_dir():
+    dest.mkdir(parents=True, exist_ok=True)
+
+  if dest.is_file():
+    dest.parent.mkdir(parents=True, exist_ok=True)
+
   execute_posix_command(command)
 
 
