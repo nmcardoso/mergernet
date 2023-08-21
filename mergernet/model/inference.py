1b62d144745c8af36a7c556f80d6fce9dd4b4fb9
Natanael <natanael_magalhaes@msn.com>
2023-08-21 19:37:13 -0300

    improving predictions

--- mergernet/model/inference.py
+++ mergernet/model/inference.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict, List
 
 import pandas as pd
@@ -8,6 +9,8 @@ from mergernet.core.experiment import Experiment
 from mergernet.core.utils import load_table
 from mergernet.data.dataset import Dataset
 
+L = logging.getLogger(__name__)
+
 
 class Predictor:
   def __init__(self, model: tf.keras.Model, dataset: Dataset):
@@ -16,62 +19,61 @@ class Predictor:
     self._preds = None
 
 
-  def predict(self) -> List:
-    # select ds by dataset type: predictions or train
-    if self.dataset.config.label_column is None:
-      ds_test = self.dataset.get_preds_dataset(prepare=True)
-    else:
-      _, ds_test = self.dataset.get_fold(0)
-      ds_test = self.dataset.prepare_data(ds_test, kind='train')
+  def predict(self, upload: str = True, name: str = None, labels: Dict = None) -> List:
+    with Experiment.Tracker(name=name, job_type='pred'):
+      # select ds by dataset type: predictions or train
+      if self.dataset.config.label_column is None:
+        ds_test = self.dataset.get_preds_dataset(prepare=True)
+      else:
+        _, ds_test = self.dataset.get_fold(0)
+        ds_test = self.dataset.prepare_data(ds_test, kind='train')
 
-    # make prediction and save in instance state
-    test_preds = self.model.predict(ds_test)
-    self._preds = test_preds
+      # make prediction and save in instance state
+      L.info('Running model predictions')
+      test_preds = self.model.predict(ds_test)
+      self._preds = test_preds
 
-    return test_preds
+      if upload:
+        # get X by dataset type: predictions or train
+        if self.dataset.config.label_column is None:
+          X = self.dataset.get_X()
+          name = name or 'predictions.csv'
+        else:
+          X = self.dataset.get_X_by_fold(0, kind='test')
+          name = name or 'test_preds_fold_0.csv'
+          labels = labels or self.dataset.config.labels
 
+        # load dataset table
+        df = load_table(self.dataset.config.table_path)
+        x_col_name = self.dataset.config.image_column
 
-  def upload(self, name: str = None, labels: Dict = None):
-    # get X by dataset type: predictions or train
-    if self.dataset.config.label_column is None:
-      X = self.dataset.get_X()
-      name = name or 'predictions.csv'
-    else:
-      X = self.dataset.get_X_by_fold(0, kind='test')
-      name = name or 'test_preds_fold_0.csv'
-      labels = labels or self.dataset.config.labels
+        L.info('preds_len:', len(self._preds))
+        L.info('df_len:', len(df))
+        L.info('X_len:', len(X))
 
-    # load dataset table
-    df = load_table(self.dataset.config.table_path)
-    x_col_name = self.dataset.config.image_column
+        # filter and order the rows of dataset table with predictions
+        df = df.set_index(x_col_name).loc[X].reset_index(inplace=False)
 
-    print('preds_len:', len(self._preds))
-    print('df_len:', len(df))
-    print('X_len:', len(X))
+        L.info('df_reindex_len:', len(df))
 
-    # filter and order the rows of dataset table with predictions
-    df = df.set_index(x_col_name).loc[X].reset_index(inplace=False)
+        # append preds columns
+        for index, label in enumerate(labels):
+          y_hat = [pred[index] for pred in self._preds]
+          L.info('y_hat_len', len(y_hat))
+          df[f'prob_{label}'] = y_hat
 
-    print('df_reindex_len:', len(df))
+        # upload stamps to W&B
+        if Experiment.log_wandb:
+          sorted_df = df.sort_values('prob_merger', ascending=False)
+          imgs = []
+          for i in range(min(50, len(sorted_df))):
+            path = sorted_df[x_col_name][i]
+            prob = sorted_df['prob_merger'][i]
+            label = f'P(M) = {prob*100:.1f}'
+            img = wandb.Image(path, caption=label)
+            imgs.append(img)
+          wandb.log({'predictions': imgs})
 
-    # append preds columns
-    for index, label in enumerate(labels):
-      y_hat = [pred[index] for pred in self._preds]
-      print('y_hat_len', len(y_hat))
-      df[f'prob_{label}'] = y_hat
-
-    # upload stamps to W&B
-    if Experiment.log_wandb:
-      with Experiment.Tracker(name=name, job_type='pred'):
-        sorted_df = df.sort_values('prob_merger', ascending=False)
-        imgs = []
-        for i in range(min(50, len(sorted_df))):
-          path = sorted_df[x_col_name][i]
-          prob = sorted_df['prob_merger'][i]
-          label = f'P(M) = {prob*100:.1f}'
-          img = wandb.Image(path, caption=label)
-          imgs.append(img)
-        wandb.log({'predictions': imgs})
-
-    # upload to github
-    Experiment.upload_file_gd(name, df)
+        # upload to github
+        Experiment.upload_file_gd(name, df)
+      return test_preds
