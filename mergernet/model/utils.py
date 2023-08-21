a827075ac7e13b6a4e00b45d091558bf5176041a
Natanael <natanael_magalhaes@msn.com>
2023-08-21 08:02:41 -0300

    log

--- mergernet/model/utils.py
+++ mergernet/model/utils.py
@@ -36,6 +36,7 @@ def set_trainable_state(
 
 def load_model(name: str, exp_id: int) -> tf.keras.Model:
   path = Experiment.download_file_gd(name, exp_id)
+  L.info(f'Loading model {str(path)}')
   return tf.keras.models.load_model(path)
 
 
