b5b0612ed9cc37190df4c0a95129f1b0eb71f0fc
Natanael <natanael_magalhaes@msn.com>
2023-08-21 21:18:45 -0300

    fix path

--- mergernet/model/inference.py
+++ mergernet/model/inference.py
@@ -75,7 +75,7 @@ class Predictor:
             )
             prob = sorted_df['prob_merger'][i]
             label = f'P(M) = {prob*100:.1f}'
-            img = wandb.Image(load_image(path), caption=label)
+            img = wandb.Image(str(path.absolute()), caption=label)
             imgs.append(img)
           wandb.log({'predictions': imgs})
 
